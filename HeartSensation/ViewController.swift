9380fe5df358b2db3820bbc77687757cc854e8ee
Vallium <aalliot@student.42.fr>
2017-05-20 14:53:09 +0200

    Building ok

--- HeartSensation/ViewController.swift
+++ HeartSensation/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         simpleBluetoothIO = SimpleBluetoothIO(serviceUUID: "6e400001-b5a3-f393-e0a9-e50e24dcca9e", delegate: self)
     }
 
