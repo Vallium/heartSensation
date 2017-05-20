a47f71726f6765c9b0b09af841e4eae71400c938
Vallium <anton.alliot@gmail.com>
2017-05-20 12:27:19 +0200

    Initial Commit

---
+++ HeartSensation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartSensation
+//
+//  Created by Anton ALLIOT on 5/20/17.
+//  Copyright © 2017 HeartSensation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
