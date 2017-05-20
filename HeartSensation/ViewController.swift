7823e6103f3f84df7d058ed649d45cddf9e01e60
Vallium <anton.alliot@gmail.com>
2017-05-20 13:58:47 +0200

    first commit

--- HeartSensation/ViewController.swift
+++ HeartSensation/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    var simpleBluetoothIO: SimpleBluetoothIO!
+    var value: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        simpleBluetoothIO = SimpleBluetoothIO(serviceUUID: "6e400001-b5a3-f393-e0a9-e50e24dcca9e", delegate: self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderOnChange(_ sender: UISlider) {
+        if (self.value != Int(sender.value)) {
+            self.value = Int(sender.value)
+            simpleBluetoothIO.writeValue(value: "Vibrate:\(Int(sender.value));")
+        }
     }
-
 
 }
 
+extension ViewController: SimpleBluetoothIODelegate {
+    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: Int8) {
+        if value > 0 {
+            view.backgroundColor = UIColor.yellow
+        } else {
+            view.backgroundColor = UIColor.black
+        }
+    }
+}
